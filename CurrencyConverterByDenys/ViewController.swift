4d8a2332a0ad7b32fe7ac54a7eca65f5e869d6e3
Denys Tymokhin <den.odessa@gmail.com>
2025-01-09 18:22:12 +0100

    Initial Commit

---
+++ CurrencyConverterByDenys/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverterByDenys
+//
+//  Created by Denis Fromfontan on 09.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CurrencyConverterByDenysTests/CurrencyConverterByDenysTests.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterByDenysTests.swift
+//  CurrencyConverterByDenysTests
+//
+//  Created by Denis Fromfontan on 09.01.2025.
+//
+
+import Testing
+@testable import CurrencyConverterByDenys
+
+struct CurrencyConverterByDenysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
